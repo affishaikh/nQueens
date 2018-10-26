b4fe751cabdeda457b1f99da7fcf9f370916f644
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-10-26 13:27:09 +0530

    Introduced the function moveQueen. Currently, it checks only for verical moves.

--- nQueens.js
+++ nQueens.js
@@ -7,9 +7,29 @@ const createQueenBoard = function(queenBoardLength) {
   return queenBoard;
 }
 
+const isColumnCompleted = function(column, completedColumns) {
+  return completedColumns.includes(column);
+}
+
+const moveQueen = function(queenBoard) {
+  let completedRows = [];
+  let completedColumns = [];
+  for(let row = 0; row < queenBoard.length; row++) {
+    for(let column = 0; column < queenBoard.length && !completedRows.includes(row); column++) {
+      if(!isColumnCompleted(column, completedColumns)) {
+        queenBoard[row][column] = 'Q';
+        completedRows.push(row);
+        completedColumns.push(column);
+      }
+    }
+  }
+  return queenBoard;
+}
+
 const main = function() {
   let queenBoardLength = +process.argv[2];
-  console.log(createQueenBoard(queenBoardLength));
+  queenBoard = createQueenBoard(queenBoardLength);
+  console.log(moveQueen(queenBoard)); 
 }
 
 main();
